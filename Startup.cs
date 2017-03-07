46e36334f0ad5249a0e2c6f80da2d7f7cc828e87
TenchuHawke <hawke@unrestrainedgames.com>
2017-03-07 14:10:29 -0800

    Initial setup

--- Startup.cs
+++ Startup.cs
@@ -4,11 +4,12 @@ using Microsoft.Extensions.DependencyInjection;
 namespace YourNamespace {
     public class Startup {
         public void ConfigureServices (IServiceCollection services) {
+            services.AddSession ();
             services.AddMvc ();
         }
         public void Configure (IApplicationBuilder App) {
-
             App.UseStaticFiles ();
+            App.UseSession ();
             // Other Use statements
             App.UseMvc (); // That's it!
         }
